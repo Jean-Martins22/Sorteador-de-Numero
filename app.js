549e4c7bd6c1f75470d08d928bd23ce42126b6bc
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2024-08-19 17:38:09 -0300

    Adicionando o código do jogo

---
+++ app.js
@@ -0,0 +1,56 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    if (de >= ate){
+        alert('O campo "Do número" precisa ser menor que o valor de "Até o número". Verifique!');
+        return;
+    }
+
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+        return;
+    }
+
+    let sorteados = [];
+    let numero = [];
+
+    for (i = 0; i < quantidade; i++){
+        numero = numeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)){
+            numero = numeroAleatorio(de, ate);
+        }
+
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+
+    alterarStatusBotao();
+}
+
+function numeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
